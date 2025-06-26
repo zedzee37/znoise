d5412a89f690cf29b972a36156aed1fab661cada
carsonuderitz@gmail.com <carsonuderitz@gmail.com>
2025-06-26 12:42:28 -0400

    begun working on Get

--- noise/perlin.go
+++ noise/perlin.go
@@ -1,6 +1,7 @@
 package noise
 
 import (
+	"fmt"
 	"math"
 	"math/rand"
 	"znoise/vector"
@@ -12,10 +13,11 @@ type PerlinNoise struct {
 	Lacunarity float32
 	Gain float32
 	grid []vector.Vec2
+	gridSize uint
 	rng *rand.Rand
 }
 
-const GridSize uint = 512
+const GridSize uint = 256
 
 func generateRandomVector(rng *rand.Rand) vector.Vec2 {
 	angle := rng.Float64() * (2 * math.Pi)
@@ -41,10 +43,20 @@ func NewPerlinNoise(seed int64, octaves int, lacunarity float32, gain float32) P
 		lacunarity,
 		gain,
 		grid,
+		GridSize,	
 		rng,
 	}
 } 
 
-func (noise PerlinNoise) Get(x float64, y float64) float64 {
-	return 1.0
+func (noise PerlinNoise) Get(x float64, y float64) (float64, error) {
+	if x < 0.0 || x > 1.0 || y < 0.0 || y > 1.0 {
+		return 0.0, fmt.Errorf(
+			"Expected a x/y value between zero and one, got: %g, %g.", x, y,
+		)	
+	}
+
+	scaledX := x * float64(noise.gridSize)
+	scaledY := y * float64(noise.gridSize)
+
+	return 1.0, nil
 }
--- vector/vector2.go
+++ vector/vector2.go
@@ -4,3 +4,7 @@ type Vec2 struct {
 	X float64
 	Y float64
 }
+
+func (v1 Vec2) Dot(v2 Vec2) float64 {
+	return v1.X*v2.X + v1.Y*v2.Y
+}
